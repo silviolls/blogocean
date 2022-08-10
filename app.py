7bc4a6a65f1dfa3b6cf31f0f2caf1d0eb4caac09
silviolls <94655161+silviolls@users.noreply.github.com>
2022-08-10 09:46:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ from flask import Flask
 app = Flask("hello")
 
 @app.route("/")
-
 def hello():
     retorno = "<html><p>Hello <B>mundo</b></p></html>"
     return retorno
+
+@app.route("/contatos")
+def contato():
+    return "Danilo de Souza, email danilo@souza"
